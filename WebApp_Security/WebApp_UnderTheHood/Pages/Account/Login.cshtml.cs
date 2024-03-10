b30e6bafb55219e988e06cc786f7c5d2a9fbccea
yesienna <sunvihowell@wp.pl>
2024-03-10 17:33:30 +0100

    Created Login Page

---
+++ WebApp_Security/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = new Credential();
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
