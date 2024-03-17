a37fad57f39a9b406662b228f53114b2b9848bc1
yesienna <sunvihowell@wp.pl>
2024-03-17 17:17:12 +0100

    Added security context

--- WebApp_Security/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
+++ WebApp_Security/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Security.Claims;
 
 namespace WebApp_UnderTheHood.Pages.Account
 {
@@ -14,7 +15,20 @@ namespace WebApp_UnderTheHood.Pages.Account
         }
         public void OnPost()
         {
+            if (!ModelState.IsValid) return;
 
+            // Verify the credential
+            if (Credential.UserName == "admin" && Credential.Password == "password") //generalnie to tak nie hardkodujemy, ale w kursie tak by³o ¿eb uproœciæ :3
+            {
+                // Creating the security context
+                var claims = new List<Claim> { 
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+            }
         }
     }
 
