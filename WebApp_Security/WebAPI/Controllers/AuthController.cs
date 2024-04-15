c6219982b74f2b29c376c76d867b7dfcefb7d51d
yesienna <sunvihowell@wp.pl>
2024-04-15 20:34:37 +0200

    Added AuthController

---
+++ WebApp_Security/WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Claims;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Authenticate([FromBody]Credential credential)
+        {
+            if (credential.UserName == "admin" && credential.Password == "password") //generalnie to tak nie hardkodujemy, ale w kursie tak było żeb uprościć :3
+            {
+                // Creating the security context
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com"),
+                    new Claim("Department", "HR")
+                };
+                var expiresAt = DateTime.UtcNow.AddMinutes(10);
+
+                return Ok(new
+                {
+                    access_token = "",
+                    expires_at = expiresAt,
+                });
+            }
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access the endpoint");
+            return Unauthorized(ModelState);
+        }
+    }
+}
---
+++ WebApp_Security/WebAPI/Models/Credential.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Models
+{
+    public class Credential
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
