ebcb517fac408c1dedbe4f9a22abde4533b11e97
yesienna <sunvihowell@wp.pl>
2024-04-23 22:21:22 +0200

    Added DbContext

--- WebApp_Security/WebApp/Program.cs
+++ WebApp_Security/WebApp/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer("");
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
