890a87263a977620de9a54a167e96b29aef47235
yesienna <sunvihowell@wp.pl>
2024-04-28 14:08:49 +0200

    Emal confirmation dry run

--- WebApp_Security/WebApp/Program.cs
+++ WebApp_Security/WebApp/Program.cs
@@ -23,7 +23,9 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
     options.User.RequireUniqueEmail = true;
     options.SignIn.RequireConfirmedEmail = true;
 })
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
